d2ee1f08736c11fcd75a71c32ec0188df5c483e7
Anderson Mateus <sinothic@gmail.com>
2022-05-22 22:04:11 -0300

    chore: add accordion ability to open and close by click

--- src/components/Shared/Accordion.vue
+++ src/components/Shared/Accordion.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>My Accordion</div>
+  <div class="py-5 border-b border-solid border-brand-gray-1">
+    <div
+      class="flex flex-wrap items-center justify-between cursor-pointer select-none"
+      @click="toggleAccordion"
+    >
+      <h3 class="text-base font-semibold">Organizations</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+  </div>
+
+  <div v-if="isOpen" class="w-full mt-5">Child</div>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Accordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
 
