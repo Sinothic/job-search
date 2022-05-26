07c142febeda8b64fb9a278c7d6e5c29f0ab7fcf
Anderson Mateus <sinothic@gmail.com>
2022-05-25 22:33:35 -0300

    test: add test to header-container component

---
+++ tests/unit/components/Shared/HeaderContainer.test.js
@@ -0,0 +1,25 @@
+import { mount } from "@vue/test-utils";
+
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+
+describe("HeaderContainer", () => {
+  it("allows parent component to provide title content", () => {
+    const wrapper = mount(HeaderContainer, {
+      slots: {
+        title: "<h2> Some Title </h2>",
+      },
+    });
+
+    expect(wrapper.text()).toMatch("Some Title");
+  });
+
+  it("allows parent component to provide subtitle content", () => {
+    const wrapper = mount(HeaderContainer, {
+      slots: {
+        subtitle: "<h3> Some Subtitle </h3>",
+      },
+    });
+
+    expect(wrapper.text()).toMatch("Some Subtitle");
+  });
+});
