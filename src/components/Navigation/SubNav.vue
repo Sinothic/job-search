2914e3ec4909a3b96c9fa936edb474c0984aeebc
Anderson Mateus <sinothic@gmail.com>
2022-05-29 22:05:45 -0300

    refactor: change the source of job list

--- src/components/Navigation/SubNav.vue
+++ src/components/Navigation/SubNav.vue
@@ -3,7 +3,12 @@
     <div class="flex flex-row flex-1 justify-between items-center h-full px-8">
       <div v-if="onJobResultPage" data-test="job-count">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-        <span> <span class="text-brand-green-1">1653</span> jobs matched </span>
+        <span>
+          <span class="text-brand-green-1">{{
+            FILTERED_JOBS_BY_ORGANIZATIONS.length
+          }}</span>
+          jobs matched
+        </span>
       </div>
       <div class="flex flex-row gap-9 items-center justify-end flex-1 h-full">
         <span>Saved jobs</span>
@@ -14,12 +19,16 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { FILTERED_JOBS_BY_ORGANIZATIONS } from "@/store";
+
 export default {
   name: "SubNav",
   computed: {
     onJobResultPage() {
       return this.$route.name == "JobResults";
     },
+    ...mapGetters([FILTERED_JOBS_BY_ORGANIZATIONS]),
   },
 };
 </script>
