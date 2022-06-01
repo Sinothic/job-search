c0de21f86adfdf0f78761fdca2a302b15f38f5d8
Anderson Mateus <sinothic@gmail.com>
2022-05-31 23:10:17 -0300

    test: add test to JobFilterSidebarOrganization

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFilterSidebarOrganizations.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFilterSidebarOrganizations.test.js
@@ -3,10 +3,11 @@ import { mount } from "@vue/test-utils";
 import JobFilterSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFilterSidebarOrganizations.vue";
 
 describe("JobFilterSidebarOrganizations", () => {
-  const createConfig = ($store) => ({
+  const createConfig = ($store, $router) => ({
     global: {
       mocks: {
         $store,
+        $router,
       },
       stubs: {
         "font-awesome-icon": true,
@@ -28,21 +29,50 @@ describe("JobFilterSidebarOrganizations", () => {
     expect(organizations).toEqual(["Facebook", "Meta"]);
   });
 
-  it("comunicates that user has selected a checkbox for organization", async () => {
-    const commit = jest.fn();
-    const $store = {
-      getters: {
-        UNIQUE_ORGANIZATIONS: new Set(["Facebook", "Meta"]),
-      },
-      commit,
-    };
+  describe("when user clicks checkbox", () => {
+    it("comunicates that user has selected a checkbox for organization", async () => {
+      const commit = jest.fn();
+      const $store = {
+        getters: {
+          UNIQUE_ORGANIZATIONS: new Set(["Facebook", "Meta"]),
+        },
+        commit,
+      };
+      const $router = { push: jest.fn() };
+      const wrapper = mount(
+        JobFilterSidebarOrganizations,
+        createConfig($store, $router)
+      );
 
-    const wrapper = mount(JobFilterSidebarOrganizations, createConfig($store));
+      const clickableArea = wrapper.find("[data-test='clickable-area']");
+      await clickableArea.trigger("click");
+      const metaInput = wrapper.find("[data-test='Meta']");
+      await metaInput.setChecked();
+      expect(commit).toHaveBeenCalledWith("ADD_SELECTED_ORGANIZATIONS", [
+        "Meta",
+      ]);
+    });
 
-    const clickableArea = wrapper.find("[data-test='clickable-area']");
-    await clickableArea.trigger("click");
-    const metaInput = wrapper.find("[data-test='Meta']");
-    await metaInput.setChecked();
-    expect(commit).toHaveBeenCalledWith("ADD_SELECTED_ORGANIZATIONS", ["Meta"]);
+    it("navigates user to the first page to see new jobs that had been filtered", async () => {
+      const push = jest.fn();
+
+      const $store = {
+        getters: {
+          UNIQUE_ORGANIZATIONS: new Set(["Facebook", "Meta"]),
+        },
+        commit: jest.fn(),
+      };
+      const $router = { push };
+      const wrapper = mount(
+        JobFilterSidebarOrganizations,
+        createConfig($store, $router)
+      );
+
+      const clickableArea = wrapper.find("[data-test='clickable-area']");
+      await clickableArea.trigger("click");
+      const metaInput = wrapper.find("[data-test='Meta']");
+      await metaInput.setChecked();
+      expect(push).toHaveBeenCalledWith({ name: "JobResults" });
+    });
   });
 });
