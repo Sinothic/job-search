4f65d8f58df3d793d7322ee1cd5a43a47a6e3ec7
Anderson Mateus <sinothic@gmail.com>
2022-05-31 21:05:38 -0300

    refactor: refactor getter FILTERED_JOBS

--- src/store/getters.js
+++ src/store/getters.js
@@ -46,19 +46,10 @@ const getters = {
     if (state.selectedOrganizations.length === 0) return true;
     return state.selectedOrganizations.includes(job.organization);
   },
-  [FILTERED_JOBS](state) {
-    const noSelectedOrganization = state.selectedOrganizations.length === 0;
-    const noSelectedJobTypes = state.selectedJobTypes.length === 0;
-
+  [FILTERED_JOBS](state, getters) {
     return state.jobs
-      .filter((job) => {
-        if (noSelectedOrganization) return true;
-        return state.selectedOrganizations.includes(job.organization);
-      })
-      .filter((job) => {
-        if (noSelectedJobTypes) return true;
-        return state.selectedJobTypes.includes(job.jobType);
-      });
+      .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
+      .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
   },
 };
 
